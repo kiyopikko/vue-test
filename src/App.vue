859c0336ca8fb3adcb6cd31b08848dcd612e00e6
Kiyohiko Heima <piyo326@gmail.com>
2019-07-13 16:33:20 +0900

    Add SimpleForm

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <SimpleForm initial-value="初期値だよ" @submit="onSubmit"/>
+    <div v-if="msg" class="msg">
+      SimpleForm から受け取った値は
+      <strong>{{ msg }}</strong> です
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SimpleForm from "./components/SimpleForm.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    SimpleForm
+  },
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    onSubmit(value) {
+      this.msg = value;
+    }
   }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 30rem;
+  margin: 4rem auto 0;
+}
+
+.msg {
+  margin: 2rem 2rem 0;
+  padding-top: 2rem;
+  border-top: 1px solid #efefef;
 }
 </style>
